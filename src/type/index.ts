4fc3446e0de1850f5d55c0ddb64d7b128475a04c
annkimm <zizicoo208@naver.com>
2025-10-06 01:41:03 +0900

    feat: add type

---
+++ src/type/index.ts
@@ -0,0 +1,32 @@
+export interface Product {
+  title: string;
+  link: string;
+  image: string;
+  lprice: string;
+  hprice: string;
+  mallName: string;
+  productId: string;
+  productType: string;
+  brand: string;
+  maker: string;
+  category1: string;
+  category2: string;
+  category3: string;
+  category4: string;
+}
+
+export interface Pagination {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+  hasNext: boolean;
+  hasPrev: boolean;
+}
+
+export interface Filters {
+  search: string;
+  category1: string;
+  category2: string;
+  sort: string;
+}
